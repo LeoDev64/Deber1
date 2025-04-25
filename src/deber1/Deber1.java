9d3d88dd92dd170bee7021705b302415a6958ea3
MODULO_4 <MODULO_4@172.16.3.37>
2025-04-25 09:39:50 -0500

    Primer cambio

---
+++ src/deber1/Deber1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package deber1;
+
+/**
+ *
+ * @author MODULO_4
+ */
+public class Deber1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Leopoldo Bolaños");
+    }
+    
+}
